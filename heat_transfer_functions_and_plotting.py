bd665163e980d022b90180acd308e5d17cfc8372
Benjamin Hills <bhills@uw.edu>
2020-10-19 12:15:41 -0600

    Widgets seem to be working
    The notebook is working well enough that I want to test it in the
    binder.

--- heat_transfer_functions_and_plotting.py
+++ heat_transfer_functions_and_plotting.py
@@ -8,60 +8,54 @@ Created on Oct 18 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-import ipywidgets
 
-def plot_perturbation_figure(T0,dT,t,zs,alpha,surfacePerturbation):
+# Dimensional Constant
+spy  = 60.*60.*24.*365.24    # seconds per year (s yr-1)
+alpha = 1.09e-6                  # Thermal Diffusivity of Ice (m2 s-1), if you change this the actual value is 1.09e-6
+
+# ---------------------------------------------------------------------------------------------------------------
+
+# Function for the surface perturbation
+def surfacePerturbation(T0,dT,t,z,alpha):
+    t *= spy
+    if alpha > .1:
+        alpha /= spy
+    from scipy.special import erfc
+    # equation 2.4-7 from Carslaw and Jaegar (1959)
+    T = T0 + dT*erfc(abs(z)/(2.*np.sqrt(alpha*t)))
+    return T
+
+# Surface Temperature Harmonic Function
+def harmonicSurface(T0,Ta,t,z,omega,alpha=alpha):
+    alpha *= spy
+    # equation 2.6-8 from Carslaw and Jaegar (1959)
+    T = T0 + Ta * np.exp(-z*np.sqrt(np.pi*omega/(alpha))) * np.sin((2.*np.pi*omega*t)-z*np.sqrt(np.pi*omega/(alpha)))
+    return T
+
+# ---------------------------------------------------------------------------------------------------------------
+
+def perturbation_interactive(T0=0.,dT=1.,t=1.,zs=np.linspace(0,1000,100),
+                        alpha=alpha,surfacePerturbation=surfacePerturbation):
 
     # Run the function
     T = surfacePerturbation(T0,dT,t,zs,alpha)
 
     # Plot
     plt.figure(figsize=(4,4))
-    plt.plot(T,zs,'k')
+    l1, = plt.plot(T,zs,'k')
     plt.ylim(max(zs),min(zs))
+    plt.xlim(-1,10)
     plt.ylabel('Depth (m)');
     plt.xlabel('Ice Temperature ($^\circ$C)');
 
-
-def plot_harmonic_figures(zs,ts,T0,Ta,omega,n_profiles,harmonicSurface):
-
-    plt.figure(figsize=(4,3))
-    plt.title('Surface Temperature')
-    plt.plot(ts,T0+Ta*np.sin(2.*np.pi*omega*ts),'k');
-    plt.ylabel('$^\circ$C')
-    plt.xlabel('Years');
     plt.tight_layout()
 
-    plt.figure(figsize=(8,4))
-    plt.subplot(121)
-    # Run and plot for all the times
-    for t in ts[::len(ts)//n_profiles]:
-        # Run the function
-        T = harmonicSurface(T0,Ta,t,zs,omega)
-        # Plot
-        plt.plot(T,zs,'k',alpha=0.25+0.75*(t/max(ts)),label='%.2f'%(t))
+    return l1
 
-    plt.legend(title='Years',bbox_to_anchor=(1, 1))
-    plt.ylabel('Depth (m)');
-    plt.xlabel('Ice Temperature ($^\circ$C)');
-    plt.ylim(max(zs),min(zs));
-    plt.title('Profile at a Specific Time')
+# ---------------------------------------------------------------------------------------------------------------
 
-    plt.subplot(122)
-    # Plot the time series for all depths
-    for z in zs[::len(zs)//n_profiles]:
-        T = harmonicSurface(T0,Ta,ts,z,omega)
-        # Plot
-        plt.plot(ts,T,'k',alpha=0.25+0.75*float(z)/max(zs),label='%.0f'%z)
-
-    plt.legend(title='Depths (m)',bbox_to_anchor=(1, 1))
-    plt.ylabel('Ice Temperature ($^\circ$C)');
-    plt.xlabel('Years');
-    plt.title('Time Series at a Specific Depth');
-    plt.tight_layout()
-
-
-def plot_harmonic_widget(zs,ts,T0,Ta,omega,n_profiles,harmonicSurface):
+def harmonic_interactive(zs=np.linspace(0,20,100),ts=np.linspace(0,1,100),T0=0.,
+                    Ta=1.,omega=1.,n_profiles=10,harmonicSurface=harmonicSurface):
 
     plt.figure(figsize=(8,4))
 
@@ -74,6 +68,7 @@ def plot_harmonic_widget(zs,ts,T0,Ta,omega,n_profiles,harmonicSurface):
 
     plt.ylabel('Ice Temperature ($^\circ$C)');
     plt.xlabel('Years');
+    plt.xlim(0,1/omega)
     plt.title('Time Series at a Specific Depth');
 
     ax2 = plt.subplot(122)
@@ -87,6 +82,7 @@ def plot_harmonic_widget(zs,ts,T0,Ta,omega,n_profiles,harmonicSurface):
     plt.ylabel('Depth (m)');
     plt.xlabel('Ice Temperature ($^\circ$C)');
     plt.ylim(max(zs),min(zs));
+    plt.xlim(-1.1,1.1)
     plt.title('Profile at a Specific Time')
 
     plt.tight_layout()
